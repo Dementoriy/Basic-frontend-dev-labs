ef8a8b32dd3c2152a6a6fd971072165805150fa0
Dmitriy Vedernikov <dim01-02@mail.ru>
2021-03-31 22:27:08 +0300

    add

--- js.js
+++ js.js
@@ -1,40 +1,27 @@
-var key = new Boolean(false)                
-var username;                
-knopka.onclick = function() {
-if(key==true){
-    alert("Я это сделал, а " + username + " тебе от меня совет: \"Поступай правильно - поступай в ВятГУ!\"")
-}
-else{
-    username = prompt("Привет, скажи твоё имя.");
-    
-    if (username == "")
-    {
-        alert("Поле должно быть заполнено")
-    }
-    else
-    {
-        alert("Я это сделал, а " + username + " тебе от меня совет: \"Поступай правильно - поступай в ВятГУ!\"");
-       key = true; 
-    }
-    
-}
-}
-knopka1.onclick = function() {
+var key = new Boolean(false)
+var username;
+
+
+function knopka() {
     if(key==true){
-        alert("Я это сделал, а " + username + " тебе от меня совет: \"Поступай правильно - поступай в ВятГУ!\"")
+        Swal.fire('Я это сделал, а ' + username + ' тебе от меня совет: “ Поступай правильно — поступай в ВятГУ!”')
     }
     else{
-        username = prompt("Привет, скажи твоё имя.");
-        
-        if (username == "")
-        {
-            alert("Поле должно быть заполнено")
-        }
-        else
-        {
-            alert("Я это сделал, а " + username + " тебе от меня совет: \"Поступай правильно - поступай в ВятГУ!\"");
-           key = true; 
-        }
-        
+        Swal.fire({
+    title: 'Введите своё имя',
+    input: 'text', inputAttributes: {
+        autocapitalize: 'off'
+    },
+    showCancelButton: true,
+    cancelButtonText: 'Отмена',
+    confirmButtonText: 'Ввести',                    
+    allowOutsideClick: () => !Swal.isLoading()
+    }).then((result) => {
+    if (result.isConfirmed) {
+        Swal.fire('Я это сделал, а ' + result.value + ' тебе от меня совет: “ Поступай правильно — поступай в ВятГУ!”')
+        username=result.value;
     }
-    }
+    })
+    key=true;    
+    } 
+}
